dbe5c1a2925b809b4bccf911b9ea0599969fd548
[2기-1반] 강석영 <ksge1124@gmail.com>
2022-06-30 15:33:49 +0900

    실습문제 준비

---
+++ week1_0629/Q4 변경점의 일부분만 골라서 커밋하기/answer.sh
@@ -0,0 +1,14 @@
+# 4번과 5번 문제는 정답코드를 작성하는 파일입니다.
+# 엘리스 플랫폼에서 '실행' 버튼을 누르고 터미널에서 아래 명령어를 직접 작성해보세요.
+
+# 기본값
+cd random_quotes
+
+# 지시사항 1번을 참고하여 코드를 작성하세요.
+git add .
+git commit -m "엘리스 토끼" quotes/politics.txt quotes/science.txt 
+
+# 지시사항 2번을 참고하여 코드를 작성하세요.
+git commit -m "모자장수" quotes/music.txt quotes.py README.md
+
+
--- week1_0629/y.git 기본 설명/git.sh
+++
@@ -1,2 +0,0 @@
-https://velog.io/@janeljs/git-4
-https://github.com/yeoseon/tip-archive/issues/85
